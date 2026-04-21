cd00c455260f65e101acb852f6b926b20026e955
unknown <franciscolima0686@digitalcollege.internal>
2026-04-20 21:10:18 -0300

    primerio commit

---
+++ DOM/app.js
@@ -0,0 +1,56 @@
+// capturar elementos 
+// let h1 = document.querySelector("#titulo")
+//  let div = document.querySelectorAll capturar todos os elementos 
+
+// Escrevando dentro de tags
+// h1.innerText = "Oi som "
+// h1.innerText += " Testando 1 2 3 ... " // += juntar os textos
+
+
+//  Adicionar Classes  
+// h1.classList.add("titulo")  
+
+//  Remover classes 
+// h1.classList.remove("titulo")
+
+//  Criar tags 
+// let body = document.querySelector("body")
+
+// let p = document.createElement("p")
+// p.innerText = "qualquer coisa"
+
+//  inserir a tag pra dentro do boy *appendchil*
+// body.appendChild(p) 
+
+// Escreve dentro da tag body uma na outra tag
+//  body.innerHTML += `<p> Qualquer coisa</p>`
+
+/*Exercicios */
+
+// function Mudartexto(){
+//     let p = document.querySelector("p")
+//     p.textContent = "texto alterado"
+
+// }
+
+// function ativar(){
+//     let div = document.querySelector("div")
+//      div.classList.add("ativo")
+
+// }
+
+
+// function ativar() {
+//     let div = document.querySelector("div")
+//     div.classList.remove("ativo")
+
+// }
+
+// let body = document.querySelector("body")
+// function ativar (){
+//     let p = document.createElement("p")
+//     let div = document.querySelector("div")
+//     p.innerText = "Parágrafo criado"
+//     body.appendChild(p)
+    
+// }   
